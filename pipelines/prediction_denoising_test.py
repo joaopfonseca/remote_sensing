241b58b82f20f4e5e49dbb683961f01b2f2c21cd
joaopfonseca <jpmrfonseca@gmail.com>
2020-01-10 14:20:54 +0000

    1st version of output denoiser

--- pipelines/prediction_denoising_test.py
+++ pipelines/prediction_denoising_test.py
@@ -7,6 +7,9 @@ PROJ_PATH = '.'
 ## data manipulation and transformation
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from src.models.denoiser import DenoiserAE
+from src.preprocess.utils import get_patches, get_2Dcoordinates_matrix
 
 ################################################################################
 # CONFIGS
@@ -20,6 +23,13 @@ INTERIM_PATH = PROJ_PATH+'/data/sentinel_coimbra/interim/'
 PROCESSED_PATH = DATA_PATH+'../processed/'
 CSV_PATH = PROCESSED_PATH+'classification_results_resnet.csv'
 
+## denoiser configs
+window_size  = 33
+n_splits_denoiser = 50
+n_epochs = 3
+
+random_state = 0
+
 ################################################################################
 # DATA READING AND PREPROCESSING
 ################################################################################
@@ -27,3 +37,31 @@ CSV_PATH = PROCESSED_PATH+'classification_results_resnet.csv'
 df = pd.read_csv(CSV_PATH).drop(columns=['Unnamed: 0'])
 noisy_lookup = df.pivot('x','y','y_pred').fillna(-1).values
 clean_lookup = df.pivot('y','x','Megaclasse').fillna(-1).values
+
+coords = get_2Dcoordinates_matrix(noisy_lookup.shape, window_size=window_size)
+
+coords = df[['y','x']]
+labels = df[['Megaclasse']]
+criteria = (
+    coords>coords.min()+int(window_size/2)+1
+    ).all(axis=1) & (
+    coords<coords.max()-int(window_size/2)
+    ).all(axis=1)
+coords = coords[criteria].astype(int).values
+labels = labels[criteria].astype(int).values
+#X = get_patches(coords, noisy_lookup, window_size)
+#y = get_patches(coords, clean_lookup, window_size)
+
+denoiser = DenoiserAE((window_size,window_size,int(clean_lookup.max())))
+#denoiser.fit(X, y, batch_size=256, epochs=1)
+
+
+print(f'Stratified Splitting: {n_splits_denoiser} splits, {int(coords.shape[0]/n_splits_denoiser)} pixels per split')
+skf = StratifiedKFold(n_splits = n_splits_denoiser, shuffle=True, random_state=random_state)
+for epoch in range(1,n_epochs+1):
+    for i, (_, split_indices) in enumerate(skf.split(coords, labels)):
+        print(f'Epoch {epoch}/{n_epochs}, Split {i+1}/{n_splits_denoiser}')
+        _coords = coords[split_indices]
+        X = get_patches(_coords, noisy_lookup, window_size)
+        y = get_patches(_coords, clean_lookup, window_size)
+        denoiser.fit(X, y, batch_size=256, epochs=1)
--- src/models/denoiser.py
+++ src/models/denoiser.py
@@ -1,80 +1,104 @@
+import numpy as np
+from keras.layers import (
+    Input,
+    Conv2D,
+    BatchNormalization,
+    Flatten,
+    Dense,
+    Reshape,
+    Conv2DTranspose,
+    Activation,
+    ZeroPadding2D
+)
+from keras.models import Model, load_model
+from keras.backend import int_shape
+from keras.callbacks import ModelCheckpoint
 
 class DenoiserAE:
-    def __init__(self, input_shape, kernel_size=3, latent_dim=16, filepath='best_model.hdf5'):
-        """input_shape: (height, width, num_bands)"""
-        self.height, self.width = input_shape
-        self.output_units = output_units
+    def __init__(self, input_shape, kernel_size=3, latent_num_distributions=10, filepath='best_model.hdf5'):
+        """
+        input_shape: (height, width, num_classes)
+        latent_num_distributions: number of distributions per class in the latent space
+        """
+        self.height, self.width, self.num_classes = input_shape
         self.kernel_size = kernel_size
-        self.latent_dim = latent_dim
+        # maybe doing it this way doesn't make sense, I'll definitely need to review this part
+        self.latent_num_distributions = latent_num_distributions
+
 
         # Encoder/Decoder number of CNN layers and filters per layer
-        layer_filters = [32, 64]
+        self.layer_filters = [32, 64]
 
         ## input layer
         self.input_layer = Input(
             (
                 self.height,
-                self.width
+                self.width,
+                self.num_classes
             )
         )
 
         ## encoder
         encoder = self._encoder(self.input_layer)
         ## latent space
-        self.latent_input = Input(shape=(self.latent_dim,), name='decoder_input')
+        self.latent_input = Input(shape=(self.latent_num_distributions*self.num_classes,), name='decoder_input')
         ## decoder
         decoder = self._decoder(self.latent_input)
 
         # Autoencoder = Encoder + Decoder
         # Instantiate Autoencoder Model
-        self.model = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+        self.model = Model(self.input_layer, decoder(encoder(self.input_layer)), name='autoencoder')
         self.model.summary()
         self.model.compile(loss='mse', optimizer='adam')
+        abspath = os.path.abspath('.')
+        self.filepath = os.path.abspath(os.path.join(abspath,filepath))
+        checkpoint = ModelCheckpoint(self.filepath, monitor='accuracy', verbose=1, save_best_only=True, mode='max')
+        self.callbacks_list = [checkpoint]
 
     def _encoder(self, input_layer):
         x = input_layer
-        for filters in layer_filters:
+        for filters in self.layer_filters:
             x = Conv2D(filters=filters,
-                       kernel_size=kernel_size,
+                       kernel_size=self.kernel_size,
                        strides=2,
                        activation='relu',
                        padding='same')(x)
-            x = BatchNormalization(x)
+            x = BatchNormalization()(x)
 
         # Shape info needed to build Decoder Model
-        self._shape = K.int_shape(x)
+        self._shape = int_shape(x)
 
         # Generate the latent vector
         x = Flatten()(x)
-        latent = Dense(self.latent_dim, name='latent_vector')(x)
-
+        latent = Dense(self.latent_num_distributions*self.num_classes, name='latent_vector')(x)
         # Instantiate Encoder Model
-        encoder = Model(inputs, latent, name='encoder')
+        encoder = Model(input_layer, latent, name='encoder')
         encoder.summary()
         return encoder
 
-    def _decoder(self, latent_input):
+    def _decoder(self, latent_inputs):
 
-        x = Dense(shape[1] * shape[2] * shape[3])(latent_input)
-        x = Reshape((shape[1], shape[2], shape[3]))(x)
+        x = Dense(self._shape[1] * self._shape[2] * self._shape[3])(latent_inputs)
+        x = Reshape((self._shape[1], self._shape[2], self._shape[3]))(x)
 
         # Stack of Transposed Conv2D blocks
         # Notes:
         # 1) Use Batch Normalization before ReLU on deep networks
         # 2) Use UpSampling2D as alternative to strides>1
         # - faster but not as good as strides>1
-        for filters in layer_filters[::-1]:
+        for filters in self.layer_filters[::-1]:
             x = Conv2DTranspose(filters=filters,
-                                kernel_size=kernel_size,
+                                kernel_size=self.kernel_size,
                                 strides=2,
                                 activation='relu',
                                 padding='same')(x)
 
-        x = Conv2DTranspose(filters=1,
-                            kernel_size=kernel_size,
+        x = Conv2DTranspose(filters=self.num_classes,
+                            kernel_size=self.kernel_size,
                             padding='same')(x)
 
         outputs = Activation('sigmoid', name='decoder_output')(x)
+        _, self._out_height, self._out_width, _ = int_shape(outputs)
 
         # Instantiate Decoder Model
         decoder = Model(latent_inputs, outputs, name='decoder')
@@ -88,16 +112,14 @@ class DenoiserAE:
 
     def fit(self, X, y, batch_size=256, epochs=100):
         # transform matrices to correct format
-        X = X.reshape(
-            -1,
-            self.height,
-            self.width
-        )
+        X = X.reshape(-1, self.height, self.width)
+        y = y.reshape(-1, self.height, self.width)
 
-        y = y.reshape(
-            -1,
-            self.height,
-            self.width,
+        X = np.array([np.moveaxis(np.array([arr==i for i in range(self.num_classes)]),0,-1) for arr in X])
+        y = np.array([np.moveaxis(np.array([arr==i for i in range(self.num_classes)]),0,-1) for arr in y])
+        y = np.pad(
+            y,
+            ((0,0), (self._out_height-self.height, 0), (self._out_width-self.width, 0), (0,0)),
         )
 
         self.history = self.model.fit(
@@ -107,7 +129,6 @@ class DenoiserAE:
             epochs=epochs,
             callbacks=self.callbacks_list
         )
-
 
     def predict(self, X, filepath=None):
         if filepath:
