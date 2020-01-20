6c8bbcdf7f71952dbce0c5fd4f0e032d14358cb8
joaopfonseca <jpmrfonseca@gmail.com>
2020-01-20 11:28:50 +0000

    Updates on full experiment

--- pipelines/pre_full_experiment.py
+++ pipelines/pre_full_experiment.py
@@ -24,6 +24,7 @@ from sklearn.ensemble import RandomForestClassifier
 ## configs
 DATA_PATH = PROJ_PATH+'/data/DGT/central_pt/'
 RAW_PATH = DATA_PATH+'raw/'
+PROCESSED_PATH = DATA_PATH+'processed/'
 TRAIN_DATA = RAW_PATH+'training.csv'
 TEST_PATH = RAW_PATH+'testing.csv'
 LABELS_PATH = RAW_PATH+'Class_legend.txt'
@@ -48,14 +49,16 @@ X = scaler.transform(X)
 ################################################################################
 ## feature selection
 # Split data into 40% test and 60% training
-_X_tr, _X_te, _y_tr, _y_te = train_test_split(X, y, test_size=0.4, random_state=random_state)
+_X_tr, _X_te, _y_tr, _y_te = train_test_split(X, y, test_size=0.4,
+    random_state=random_state)
 
 # Create and train a random forest classifier
-clf = RandomForestClassifier(n_estimators=1000, n_jobs=-1, random_state=random_state)
+clf = RandomForestClassifier(n_estimators=1000, n_jobs=-1,
+    random_state=random_state)
 clf.fit(_X_tr, _y_tr)
 
 # Gini Index Importance Feature Selection Method
-gini_imp_feat_sel = SelectFromModel(clf, prefit=True, threshold='.9*mean')
+gini_imp_feat_sel = SelectFromModel(clf, prefit=True, threshold='.8*mean')
 gini_accepted = gini_imp_feat_sel.get_support()
 
 # Permutation
@@ -64,7 +67,18 @@ imp = permutation_importances(
     pd.DataFrame(_X_te, columns=feat_labels),
     pd.Series(_y_te, name='CLASS')
 )
-viz = plot_importances(imp)
+permutation_accepted = (imp['Importance']>0).loc[feat_labels].values
+
+# Keep the ones accepted with both methods
+accepted_feats = (gini_accepted.astype(int)+permutation_accepted.astype(int))==2
+
+# save feature selection results
+feat_sel_results = pd.DataFrame(
+    np.array([gini_accepted, permutation_accepted, accepted_feats]).T,
+    index=feat_labels,
+    columns=['Gini', 'Permutation', 'Selected']
+)
+feat_sel_results.to_csv(PROCESSED_PATH+'feature_selection_results.csv')
 
 
 ################################################################################
